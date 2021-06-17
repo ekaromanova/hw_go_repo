439b47a531639c486691b7717a461524e1084b67
ekaromanova <ekaromanova@ozon.ru>
2021-06-17 23:52:01 +0400

    add text

--- hw4.go
+++ hw4.go
@@ -28,36 +28,3 @@ func main() {
 }
 
 
-
-
-
-package main
-
-import (
-"bufio"
-"fmt"
-"os"
-"strconv"
-)
-
-func main() {
-	inputNums := [5]int64{}
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// записываем введённые числа в массив в обратном порядке
-	for i := len(inputNums) - 1; i >= 0; i-- {
-		if scanner.Scan() {
-			num, err := strconv.ParseInt(scanner.Text(), 10, 64)
-			if err != nil {
-				panic(err)
-			}
-			inputNums[i] = num
-		} else {
-			panic("you must input 5 numbers")
-		}
-	}
-	for _, el := range inputNums {
-		fmt.Println(el)
-	}
-}
-
